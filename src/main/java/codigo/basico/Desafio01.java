d3f5211dc3df1d4c80b0054b85c3d31ae5109071
Mislaine <a.s.mislaine@gmail.com>
2023-09-18 14:55:45 -0300

    desafios basico 1e2

---
+++ src/main/java/codigo/basico/Desafio01.java
@@ -0,0 +1,22 @@
+package codigo.basico;
+
+import java.util.Scanner;
+
+public class Desafio01 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Saldo atual: ");
+        double saldoAtual = scanner.nextDouble();
+
+        System.out.println("Valor depositado: ");
+        double valorDeposito = scanner.nextDouble();
+
+        System.out.println("Valor do saque: ");
+        double valorRetirada = scanner.nextDouble();
+
+        double novoSaldo = saldoAtual + (valorDeposito - valorRetirada);
+
+        System.out.printf("Saldo atualizado na conta: %.1f\n", novoSaldo);
+    }
+}
---
+++ src/main/java/codigo/basico/Desafio02.java
@@ -0,0 +1,28 @@
+package codigo.basico;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Scanner;
+
+public class Desafio02 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        ArrayList<String> ativos = new ArrayList<>();
+
+        System.out.println("Digite a quantidade de ativos");
+        int quantidadeAtivos = scanner.nextInt();
+        // Limpar a nova linha após o número inteiro
+        scanner.nextLine();
+
+        // Entrada dos códigos dos ativos
+        for (int i = 0; i < quantidadeAtivos; i++) {
+            String codigoAtivo = scanner.nextLine();
+            ativos.add(codigoAtivo);
+        }
+        Collections.sort(ativos);
+
+        for (String ativo : ativos) {
+            System.out.println(ativo);
+        }
+    }
+}
