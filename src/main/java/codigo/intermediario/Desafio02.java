52973725aee35fd75be5aa6fdc08a3646a32dbdf
Mislaine <a.s.mislaine@gmail.com>
2023-09-19 16:12:59 -0300

    desafio02

--- src/main/java/codigo/intermediario/Desafio02.java
+++ src/main/java/codigo/intermediario/Desafio02.java
@@ -1,4 +1,62 @@
 package codigo.intermediario;
 
+import java.text.DecimalFormat;
+import java.util.Scanner;
+
 public class Desafio02 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Digite o nome do titular da conta: ");
+        String titular = scanner.nextLine();
+
+        System.out.print("Digite o número da conta: ");
+        int numeroConta = scanner.nextInt();
+
+        System.out.print("Saldo bancário: ");
+        double saldo = scanner.nextDouble();
+
+        System.out.print("Taxa de juros da conta poupança (%): ");
+        double taxaJuros = scanner.nextDouble();
+
+        ContaPoupanca contaPoupanca = new ContaPoupanca(numeroConta, titular, saldo, taxaJuros);
+        System.out.println("Conta Poupanca:");
+        contaPoupanca.exibirInformacoes();
+    }
 }
+
+class ContaBancaria2 {
+    protected int numero;
+    protected String titular;
+    protected double saldo;
+
+    public ContaBancaria2(int numero, String titular, double saldo) {
+        this.numero = numero;
+        this.titular = titular;
+        this.saldo = saldo;
+    }
+
+    public void exibirInformacoes() {
+        DecimalFormat decimalFormat = new DecimalFormat("#.0");
+        System.out.println(titular);
+        System.out.println(numero);
+        System.out.println("Saldo: R$ " + decimalFormat.format(saldo));
+        
+    }
+}
+
+class ContaPoupanca extends ContaBancaria2 {
+    private double taxaJuros;
+
+    public ContaPoupanca(int numero, String titular, double saldo, double taxaJuros) {
+        super(numero, titular, saldo);
+        this.taxaJuros = taxaJuros;
+    }
+
+    public void exibirInformacoes() {
+        super.exibirInformacoes();
+        System.out.println("Taxa de Juros: " + taxaJuros + "%");
+    }
+}
+
+
